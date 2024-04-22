ea1d22b847cd02f9ce5f983735457e050f384feb
dir-FY24kemmochiakane <akane.kenmochi.u888@dirbato.co.jp>
2024-04-22 16:48:32 +0900

    ShowServlet.javaを作成

--- message_board/src/filters/EncodingFilter.java
+++ message_board/src/filters/EncodingFilter.java
@@ -34,19 +34,15 @@ public class EncodingFilter extends HttpFilter implements Filter {
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+	 public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	            throws IOException, ServletException {
+	        request.setCharacterEncoding("UTF-8");
+	        response.setCharacterEncoding("UTF-8");
 
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+	        chain.doFilter(request, response);
+	    }
+
+	    public void init(FilterConfig fConfig) throws ServletException {
+	    }
+
 	}
-
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
-
-}
